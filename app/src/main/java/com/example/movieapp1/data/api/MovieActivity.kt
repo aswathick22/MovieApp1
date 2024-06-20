66806c62ffdd14dd4ca92d306dd1ccc9009a5324
aswathiram19 <ackandambeth@gmail.com>
2024-06-20 08:37:47 +0530

    Layout files added

--- app/src/main/java/com/example/movieapp1/data/api/MovieActivity.kt
+++ app/src/main/java/com/example/movieapp1/data/api/MovieActivity.kt
@@ -7,33 +7,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.GridView
-import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 
-private var GridView.layoutManager: LinearLayoutManager
-    get() {}
-    set(value) {}
-
-class MovieFragment : Fragment() {
+class MovieActivity : AppCompatActivity() {
 
     private val movieViewModel by viewModels<MovieViewModel>()
+    private val movieRepository : MovieDetailsRepository
 
-    override fun onCreateView(
+    fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.movie_grid, container, false)
+        return inflater.inflate(R.layout.activity_movie, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val movieView: GridView = view.findViewById(R.id.)
         movieView.layoutManager = LinearLayoutManager(context)
 
         movieViewModel.movieId.observe(viewLifecycleOwner){ items ->
-            movieView.adapter = MovieAdaptor(items)
+            movieView.adapter = MovieAdapter(items)
         }
 
     }
